8f49929365c50774caed7fa9f0b9d58fe520912d
Veldrine Evelia Kaharwa <eveliaveldrine@outlook.com>
2025-07-08 15:10:55 +0300

    update code

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://tech-vexy.github.io',
-	base: '/blog',
+	base: '/',
 	integrations: [mdx(), sitemap()],
 	build: {
 		inlineStylesheets: 'auto',
